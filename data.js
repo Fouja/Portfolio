19cd7a34fd44cf211787c472173bc42bf7268d6c
fouja <foujanazgull@gmail.com>
2026-08-24 16:31:25 -0400

    feat: transparent hero photo with neural-network/binary background
    - Remove background from hero profile photo (fouad-hammani-nobg.png)
    - Add animated canvas neural network + binary rain behind the hero photo
    - Use separate hero_photo data field for transparent image
    - Update 3D card styles to let background show through
    - Ignore .venv in .gitignore

--- data.js
+++ data.js
@@ -21,6 +21,7 @@ window.PORTFOLIO_DATA = {
     years_experience: 7,
       organizations_worked: 5,
       photo: "./assets/fouad-hammani.webp",
+      hero_photo: "./assets/fouad-hammani-nobg.png",
       badge_photo: "./assets/fouad-hammani.webp",
       logo: "./assets/foujalab.webp",
       resume_url: "./assets/Fouad_Hammani_Engineer_Canada_ATS.pdf"
